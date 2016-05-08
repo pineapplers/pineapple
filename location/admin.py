b41718feff1d71c32690d83585cbcde0c143abba
WenTao <tonnie.lwt@gmail.com>
2016-05-08 21:23:26 +0800

    添加地理模块

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Province, City
+
 
 # Register your models here.
+class CityAdmin(admin.TabularInline):
+    model = City
+
+
+class ProvinceAdmin(admin.ModelAdmin):
+    model = Province
+
+    inlines = [
+        CityAdmin,
+    ]
+
+
+admin.site.register(Province, ProvinceAdmin)
--- location/models.py
+++ location/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Province(models.Model):
+    name = models.CharField(max_length=32, verbose_name='省份名称')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '省份'
+        verbose_name_plural = '省份'
+
+
+class City(models.Model):
+    name = models.CharField(max_length=32, verbose_name='城市名称')
+    province = models.ForeignKey(Province, verbose_name='省份')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '城市'
+        verbose_name_plural = '城市'
