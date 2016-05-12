06dee79852f792fcd0ca83c2bcdbf77800ad4d40
tonnie17 <tonnie.lwt@gmail.com>
2016-05-12 17:24:20 +0800

    更新actions模块

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Action
+
 # Register your models here.
+class ActionAdmin(admin.ModelAdmin):
+    model = Action
+
+
+admin.site.register(Action, ActionAdmin)
