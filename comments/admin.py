3a5ae423cf2d1382d590358d278622ee9441e2ce
tonnie17 <tonnie.lwt@gmail.com>
2016-05-12 17:24:43 +0800

    更新评论模块

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Comment
+
 # Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
+admin.site.register(Comment, CommentAdmin)
--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+from food.models import FoodItem
 from user.models import User
 
 # Create your models here.
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='评论用户')
     content = models.CharField(max_length=512, verbose_name='评论内容')
+    food = models.ForeignKey(FoodItem, related_name='comments', verbose_name='所属美食')
     created = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
