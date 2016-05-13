ed7a3f0572d11c0f5bc8accadc143823ccf0ada1
WenTao <tonnie.lwt@gmail.com>
2016-05-13 23:05:40 +0800

    添加博客模块

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128, verbose_name='标题')
+    user = models.ForeignKey(User, related_name='posts', verbose_name='用户')
+    body = models.TextField(verbose_name='内容')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='创建日期')
+
+    class Meta:
+        verbose_name = '博客'
+        verbose_name_plural = '博客'
