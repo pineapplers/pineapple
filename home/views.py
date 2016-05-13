66a772bd1c4bc07976df7037d9974908416bdad6
WenTao <tonnie.lwt@gmail.com>
2016-05-13 23:46:38 +0800

    fix

--- home/views.py
+++ home/views.py
@@ -9,17 +9,17 @@ from search.forms import SearchForm
 @cache_page(60)
 def home(request):
     form = SearchForm()
-    actions = Action.objects.all().exclude(user=request.user)
-    following_ids = request.user.following.values_list('id', flat=True)
-    if following_ids:
-        actions = actions.filter(user_id__in=following_ids).select_related('user', 'user__profile').prefetch_related('target')
-    actions = actions[:10]
-
     if request.user.is_authenticated():
-        pass
-    else:
-        pass
+        actions = Action.objects.all().exclude(user=request.user)
+        following_ids = request.user.following.values_list('id', flat=True)
+        if following_ids:
+            actions = actions.filter(user_id__in=following_ids).select_related('user', 'user__profile').prefetch_related('target')
+        actions = actions[:10]
+
+        return render(request, 'home/index.tpl', {
+                'search_form': form,
+                'actions': actions
+           })
     return render(request, 'home/index.tpl', {
-            'search_form': form,
-            'actions': actions
-       })
+        'search_form': form,
+    })
