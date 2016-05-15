ae5e3dd40d8c41f0ae2203124fe8294bbd8b690c
WenTao <tonnie.lwt@gmail.com>
2016-05-15 22:22:40 +0800

    更新

--- food/urls.py
+++ food/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
-from .views import food_detail, food_like, food_category, food_tag, foods_most_popular
+from .views import food_detail, food_like, food_category, food_tag, explore, food_wta, food_ate
 
 urlpatterns = [
     url(r'^(?P<food_id>[\d]+)/$', food_detail, name='detail'),
     url(r'^food_like/$', food_like, name='like'),
+    url(r'^food_wta/$', food_wta, name='wta'),
+    url(r'^food_ate/$', food_ate, name='ate'),
     url(r'^category/(?P<category>[\w]+)/$', food_category, name='category'),
     url(r'^tag/(?P<tag>)/$', food_tag, name='tag'),
-    url(r'^most_popular/$', foods_most_popular, name='most-popular'),
+    url(r'^explore/$', explore, name='explore'),
 ]
